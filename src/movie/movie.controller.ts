f4816c2f796f3f0696c35c5c463b1cef127cd69e
Hassan-Ali-CS <ihassanalli@proton.me>
2025-03-06 21:52:36 +0500

    Add Guards to movie.controller.ts

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,33 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query, BadRequestException } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, BadRequestException, UseGuards } from '@nestjs/common';
 import { MovieService } from './movie.service';
 import { CreateMovieDto } from './DTO/create-movie.dto';
 import { updateMovieDto } from './DTO/update-movie.dto';
+import { JwtAuthGuard } from 'src/auth/jwt-auth/jwt-auth.guard';
 
 @Controller('movie')
 export class MovieController {
     constructor(private readonly movieService: MovieService) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post()
     async create(@Body() createMovieDto: CreateMovieDto) {
         return this.movieService.create(createMovieDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get()
     findAll() {
         return this.movieService.findAll();
     }
 
-
+    @UseGuards(JwtAuthGuard)
     @Patch(':id')
     update(@Param('id') id: number, @Body() updateMovieDto: updateMovieDto) {
         return this.movieService.update(id, updateMovieDto);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete(':id')
     delete(@Param('id') id: number) {
         return this.movieService.remove(id);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('search')
     async searchMovies(@Query('keyword') keyword: string) {
         if (!keyword) {
@@ -36,6 +41,7 @@ export class MovieController {
         return this.movieService.searchMovies(keyword);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get(':id') //  Ensure this only works for numbers
         findOne(@Param('id') id: string) {
             if (isNaN(Number(id))) {
