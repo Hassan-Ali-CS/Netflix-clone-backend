31be7d749956431f0d997b47f3695090561ca054
Hassan-Ali-CS <ihassanalli@proton.me>
2025-03-06 19:50:19 +0500

    Add guards to subscription.controller.ts

--- src/subscription/subscription.controller.ts
+++ src/subscription/subscription.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Param, Delete, Patch } from "@nestjs/common";
+import { Controller, Get, Post, Body, Param, Delete, Patch, UseGuards } from "@nestjs/common";
 import { SubscriptionService } from "./subscription.service";
 import { CreateSubscriptionDto } from "./dto/create-subscription.dto";
 import { UpdateSubscriptionDto } from "./dto/update-subscription.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth/jwt-auth.guard";
 
 @Controller("subscriptions")
 export class SubscriptionController {
   constructor(private readonly subscriptionService: SubscriptionService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createSubscriptionDto: CreateSubscriptionDto) {
     return this.subscriptionService.create(createSubscriptionDto);
@@ -22,16 +24,19 @@ export class SubscriptionController {
     return this.subscriptionService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post(":id/subscribe/:userId")
   subscribeUser(@Param("userId") userId: number, @Param("id") subscriptionId: number) {
     return this.subscriptionService.subscribeUser(userId, subscriptionId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(":id")
   update(@Param("id") id: number, @Body() updateSubscriptionDto: UpdateSubscriptionDto) {
     return this.subscriptionService.update(id, updateSubscriptionDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(":id")
   remove(@Param("id") id: number) {
     return this.subscriptionService.remove(id);
